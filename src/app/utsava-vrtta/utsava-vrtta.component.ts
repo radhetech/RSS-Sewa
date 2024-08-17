74a8d441188a1380145742b535ba6c36fe94d0c9
dainik <dainiksutariya0505@gmail.com>
2024-08-17 09:29:33 -0400

    utsava-vrtta-form done

--- src/app/utsava-vrtta/utsava-vrtta.component.ts
+++ src/app/utsava-vrtta/utsava-vrtta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-utsava-vrtta',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './utsava-vrtta.component.scss'
 })
 export class UtsavaVrttaComponent {
+
+
+  rakshabandhanForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.rakshabandhanForm = this.fb.group({
+      nagarName: [''],
+      utsavCount: [''],
+      totalSevaPopulation: [''],
+      contactedSevaPopulation: [''],
+      workersCount: [''],
+      familyContacted: [''],
+      rakshaOrMessageGiven: [''],
+      additionalContacts: [''],
+      newWorkers: [''],
+      specialNotes: ['']
+    });
+  }
+
+  onSubmit(e:any) {
+    console.log(e.value);
+    e.reset()
+  }
 
 }
