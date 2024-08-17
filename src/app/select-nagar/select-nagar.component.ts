b9b3e413d418213b83eff387e79d6575ed8b05cd
Your Name <you@example.com>
2024-08-17 22:46:59 +0530

    vishal css changes

--- src/app/select-nagar/select-nagar.component.ts
+++ src/app/select-nagar/select-nagar.component.ts
@@ -7,97 +7,99 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-select-nagar',
   templateUrl: './select-nagar.component.html',
-  styleUrl: './select-nagar.component.scss'
+  styleUrl: './select-nagar.component.scss',
 })
 export class SelectNagarComponent {
-  adminForm:FormGroup;
-  vibhagList:any = [];
-  jillaList:any = [];
-  talukaList:any = [];
-  vastiList:any = [];
-  shakhaList:any = [];
-  selectedNagar:any =[];
+  adminForm: FormGroup;
+  vibhagList: any = [];
+  jillaList: any = [];
+  talukaList: any = [];
+  vastiList: any = [];
+  shakhaList: any = [];
+  selectedNagar: any = [];
 
   // sharedService=inject()
-  router=inject(Router)
-  getVibhagUrl:string = 'http://localhost:4000/vibhagList';
+  router = inject(Router);
+  getVibhagUrl: string = 'http://localhost:4000/vibhagList';
 
- constructor(private ApiService:ApiService){
-   this.adminForm = new FormGroup({
-    vibhag: new FormControl(''),
-    jilla: new FormControl(''),
-    taluka: new FormControl(''),
-    vasti: new FormControl(''),
-    shakha: new FormControl(''),
-   })
- }
+  constructor(private ApiService: ApiService) {
+    this.adminForm = new FormGroup({
+      vibhag: new FormControl(''),
+      jilla: new FormControl(''),
+      taluka: new FormControl(''),
+      vasti: new FormControl(''),
+      shakha: new FormControl(''),
+    });
+  }
 
- ngOnInit(){
-   this.ApiService.getData(this.getVibhagUrl).subscribe({next:(res:any)=>{
-    console.log(res);
-    this.vibhagList = res["vibhag"]
-   },error:()=>{}})
- }
- vibhagChange(e:any){
-  this.jillaList=[];
-  this.talukaList=[];
-  this.vastiList=[];
-  this.shakhaList = [];
-  const selectedVibhag = e.target.value;
-   this.vibhagList.forEach((item:any)=>{
-   if(item.name==e.target.value){
-     this.jillaList = item.jilla;
-   }
-   this.selectedNagar.vibhag = selectedVibhag;
-
-})
- }
- jillaChange(e:any){
-  this.talukaList=[];
-  this.vastiList=[];
-  this.shakhaList = [];
-  const selectedJilla = e.target.value;
-  this.jillaList.forEach((item:any)=>{
-  if(item.name==e.target.value){
-    this.talukaList = item.taluka;
+  ngOnInit() {
+    this.ApiService.getData(this.getVibhagUrl).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.vibhagList = res['vibhag'];
+      },
+      error: () => {},
+    });
   }
-})
-this.selectedNagar.jilla = selectedJilla;
-}
-talukaChange(e:any){
-  this.vastiList=[];
-  this.shakhaList = [];
-  const selectedTaluka = e.target.value;
-  this.talukaList.forEach((item:any)=>{
-  if(item.name==e.target.value){
-    this.vastiList = item.sevaVasti;
+  vibhagChange(e: any) {
+    this.jillaList = [];
+    this.talukaList = [];
+    this.vastiList = [];
+    this.shakhaList = [];
+    const selectedVibhag = e.target.value;
+    this.vibhagList.forEach((item: any) => {
+      if (item.name == e.target.value) {
+        this.jillaList = item.jilla;
+      }
+      this.selectedNagar.vibhag = selectedVibhag;
+    });
   }
-})
-this.selectedNagar.taluka = selectedTaluka;
-}
-vastiChange(e:any){
-  this.shakhaList = [];
-  const selectedVasti = e.target.value;
-  this.vastiList.forEach((item:any)=>{
-  if(item.name==e.target.value){
-    this.shakhaList = item.shakha;
+  jillaChange(e: any) {
+    this.talukaList = [];
+    this.vastiList = [];
+    this.shakhaList = [];
+    const selectedJilla = e.target.value;
+    this.jillaList.forEach((item: any) => {
+      if (item.name == e.target.value) {
+        this.talukaList = item.taluka;
+      }
+    });
+    this.selectedNagar.jilla = selectedJilla;
   }
-})
-this.selectedNagar.vasti = selectedVasti;
-}
-shakhaChange(e:any){
-  this.selectedNagar.shakha = e.target.value;
-}
+  talukaChange(e: any) {
+    this.vastiList = [];
+    this.shakhaList = [];
+    const selectedTaluka = e.target.value;
+    this.talukaList.forEach((item: any) => {
+      if (item.name == e.target.value) {
+        this.vastiList = item.sevaVasti;
+      }
+    });
+    this.selectedNagar.taluka = selectedTaluka;
+  }
+  vastiChange(e: any) {
+    this.shakhaList = [];
+    const selectedVasti = e.target.value;
+    this.vastiList.forEach((item: any) => {
+      if (item.name == e.target.value) {
+        this.shakhaList = item.shakha;
+      }
+    });
+    this.selectedNagar.vasti = selectedVasti;
+  }
+  shakhaChange(e: any) {
+    this.selectedNagar.shakha = e.target.value;
+  }
 
   OnCloseForm() {
     // this.router.navigate(['/SevaUpakrama'])
   }
 
-  OnFormSubmitted(){
+  OnFormSubmitted() {
     // if (this.selectedNagar) {
     //   this.sharedService.changeNagar(this.selectedNagar);
     //   console.log('Form submitted with nagar:', this.selectedNagar);
     // }
-    this.router.navigate(['/SevaUpakrama'])
+    this.router.navigate(['/SevaUpakrama']);
   }
 }
