1d1f74654ca2e13cdc9de7633f35aa162db7215b
Your Name <you@example.com>
2024-08-18 18:11:32 +0530

    Vishal changes 18.08

--- src/app/new-shakha-vrut/new-shakha-vrut.component.ts
+++ src/app/new-shakha-vrut/new-shakha-vrut.component.ts
@@ -6,185 +6,197 @@ import { ApiService } from '../services/api.service';
 @Component({
   selector: 'app-new-shakha-vrut',
   templateUrl: './new-shakha-vrut.component.html',
-  styleUrl: './new-shakha-vrut.component.scss'
+  styleUrl: './new-shakha-vrut.component.scss',
 })
 export class NewShakhaVrutComponent implements OnInit {
-  formGroup!:FormGroup;
+  formGroup!: FormGroup;
   lastFiveThursdays: Date[] = [];
-  isShakhaSelected:boolean = false;
-  isVastiSelected:boolean = false;
-  dateHeading:any='4/4/66464';
-  userData:any;
-  vrutVasti:string = '';
-  vrutShakha:string = '';
-  selectedDate:any;
-  formId:string = '';
-  formType:string = 'Submit';
-  sanskar1List:any = [{name:'amrutvachan/subhashit'},{name:'seva git'},
-  {name:'seva bodh katha'}
-]
+  isShakhaSelected: boolean = false;
+  isVastiSelected: boolean = false;
+  dateHeading: any = '4/4/66464';
+  userData: any;
+  vrutVasti: string = '';
+  vrutShakha: string = '';
+  selectedDate: any;
+  formId: string = '';
+  formType: string = 'સબમિટ';
+  sanskar1List: any = [
+    { name: 'amrutvachan/subhashit' },
+    { name: 'seva git' },
+    { name: 'seva bodh katha' },
+  ];
   isSanskar1Open = false;
   isSanskar2Open = false;
-constructor(private valueSel:valueSelect, private _apiService:ApiService){
-  this.getLastFiveThursdays();
-  this.userData = this.valueSel.getUserData()!;
-  this.formGroup= new FormGroup({
-    // selectedDate: new FormControl(this.dateHeading,Validators.required),
-    selectedCategory: new FormControl('',Validators.required),
-    weeklyForm: new FormGroup({
-      sanskar:new FormControl(''),
-      sanskar1Group:new FormGroup({
-        sevaBodhKatha: new FormControl(''),
-        sevaGit: new FormControl(''),
-        amrutvachanSubhashit: new FormControl(''),
+  constructor(private valueSel: valueSelect, private _apiService: ApiService) {
+    this.getLastFiveThursdays();
+    this.userData = this.valueSel.getUserData()!;
+    this.formGroup = new FormGroup({
+      // selectedDate: new FormControl(this.dateHeading,Validators.required),
+      selectedCategory: new FormControl('', Validators.required),
+      weeklyForm: new FormGroup({
+        sanskar: new FormControl(''),
+        sanskar1Group: new FormGroup({
+          sevaBodhKatha: new FormControl(''),
+          sevaGit: new FormControl(''),
+          amrutvachanSubhashit: new FormControl(''),
+        }),
+        sanskar2Group: new FormGroup({
+          karyNondh: new FormControl(''),
+          vishayLenar: new FormControl(''),
+          sevaPrasang: new FormControl(''),
+          karyAyojan: new FormControl(''),
+        }),
       }),
-      sanskar2Group:new FormGroup({
-        karyNondh: new FormControl(''),
-        vishayLenar: new FormControl(''),
-       sevaPrasang: new FormControl(''),
-       karyAyojan: new FormControl('')
+      monthlyForm: new FormGroup({
+        sampark: new FormControl(''),
+        swSankhya: new FormControl(''),
+        visheshNondh: new FormControl(''),
       }),
-    }),
-    monthlyForm: new FormGroup({
-      sampark: new FormControl(''),
-      swSankhya: new FormControl(''),
-      visheshNondh: new FormControl(''),
-    })
-  })
-}
+    });
+  }
   ngOnInit(): void {
-    this.valueSel.getCurrentShakha().subscribe((res)=>{
+    this.valueSel.getCurrentShakha().subscribe((res) => {
       this.formGroup.reset();
-      if(res!='શાખા'){
+      if (res != 'શાખા') {
         this.vrutShakha = res;
-       this.isShakhaSelected = true;
+        this.isShakhaSelected = true;
       } else {
-       this.isShakhaSelected = false;
+        this.isShakhaSelected = false;
       }
-    })
-    this.valueSel.getCurrentVasti().subscribe((res)=>{
+    });
+    this.valueSel.getCurrentVasti().subscribe((res) => {
       this.formGroup.reset();
-     if(res!='વસ્તી'){
-      this.vrutVasti = res;
-      this.isVastiSelected = true;
-     } else {
-       this.isVastiSelected = false;
-     }
-   })
-  
-}
-dateSelChange(selDate:any){
-  //console.log(this.getFormattedDate(selDate));
-  // api vasti, shakha, date
-  this._apiService.getData('http://localhost:4000/shakhaVrut').subscribe((res:any)=>{
-    res = res[1];
-    this.formId = res?.id;
-  if(res!=undefined){
-    this.formType = 'Update';
-    this.formGroup.patchValue({
-      selectedCategory : res?.selectedCategory,
-      weeklyForm:{
-        sanskar:res?.weeklyForm?.sanskar,
-        sanskar1Group:{
-          sevaBodhKatha: res?.weeklyForm?.sanskar1Group?.sevaBodhKatha,
-          sevaGit: res?.weeklyForm?.sanskar1Group?.sevaGit,
-          amrutvachanSubhashit: res?.weeklyForm?.sanskar1Group?.amrutvachanSubhashit,
-        },
-        sanskar2Group:{
-          karyNondh:  res?.weeklyForm?.sanskar2Group?.karyNondh,
-          vishayLenar: res?.weeklyForm?.sanskar2Group?.vishayLenar,
-         sevaPrasang: res?.weeklyForm?.sanskar2Group?.sevaPrasang,
-         karyAyojan:res?.weeklyForm?.sanskar2Group?.karyAyojan
-        },
-      sheshNondh: res?.monthlyForm?.visheshNondh,
-      },
-      monthlyForm: {
-        sanskar:res?.monthlyForm?.sanskar,
-        sampark: res?.monthlyForm?.sampark,
-        swSankhya: res?.monthlyForm?.swSankhya,
-        visheshNondh: res?.monthlyForm?.visheshNondh
+      if (res != 'વસ્તી') {
+        this.vrutVasti = res;
+        this.isVastiSelected = true;
+      } else {
+        this.isVastiSelected = false;
       }
-    })
+    });
   }
-   
-  })
-}
-get selectedCategory(){
-  return this.formGroup.controls['selectedCategory'].value
-}
-get selectedSanskar(){
-  return (this.formGroup.controls['weeklyForm'] as FormGroup).get('sanskar')!.value;
-  //return temp.controls['sanskar'];
-}
-get samparkSelected(){
-  return (this.formGroup.controls['monthlyForm'] as FormGroup).get('sampark')!.value;
-}
-getLastFiveThursdays(): void {
-  const today = new Date();
-  let count = 0;
-  if (today.getDay() === 4) {
-    this.lastFiveThursdays.push(new Date(today)); 
-    count++;
+  dateSelChange(selDate: any) {
+    //console.log(this.getFormattedDate(selDate));
+    // api vasti, shakha, date
+    this._apiService
+      .getData('http://localhost:4000/shakhaVrut')
+      .subscribe((res: any) => {
+        res = res[1];
+        this.formId = res?.id;
+        if (res != undefined) {
+          this.formType = 'અપડેટ';
+          this.formGroup.patchValue({
+            selectedCategory: res?.selectedCategory,
+            weeklyForm: {
+              sanskar: res?.weeklyForm?.sanskar,
+              sanskar1Group: {
+                sevaBodhKatha: res?.weeklyForm?.sanskar1Group?.sevaBodhKatha,
+                sevaGit: res?.weeklyForm?.sanskar1Group?.sevaGit,
+                amrutvachanSubhashit:
+                  res?.weeklyForm?.sanskar1Group?.amrutvachanSubhashit,
+              },
+              sanskar2Group: {
+                karyNondh: res?.weeklyForm?.sanskar2Group?.karyNondh,
+                vishayLenar: res?.weeklyForm?.sanskar2Group?.vishayLenar,
+                sevaPrasang: res?.weeklyForm?.sanskar2Group?.sevaPrasang,
+                karyAyojan: res?.weeklyForm?.sanskar2Group?.karyAyojan,
+              },
+              sheshNondh: res?.monthlyForm?.visheshNondh,
+            },
+            monthlyForm: {
+              sanskar: res?.monthlyForm?.sanskar,
+              sampark: res?.monthlyForm?.sampark,
+              swSankhya: res?.monthlyForm?.swSankhya,
+              visheshNondh: res?.monthlyForm?.visheshNondh,
+            },
+          });
+        }
+      });
   }
-  while (count < 5) {
-    today.setDate(today.getDate() - 1);
-    if (today.getDay() === 4) { 
+  get selectedCategory() {
+    return this.formGroup.controls['selectedCategory'].value;
+  }
+  get selectedSanskar() {
+    return (this.formGroup.controls['weeklyForm'] as FormGroup).get('sanskar')!
+      .value;
+    //return temp.controls['sanskar'];
+  }
+  get samparkSelected() {
+    return (this.formGroup.controls['monthlyForm'] as FormGroup).get('sampark')!
+      .value;
+  }
+  getLastFiveThursdays(): void {
+    const today = new Date();
+    let count = 0;
+    if (today.getDay() === 4) {
       this.lastFiveThursdays.push(new Date(today));
       count++;
     }
+    while (count < 5) {
+      today.setDate(today.getDate() - 1);
+      if (today.getDay() === 4) {
+        this.lastFiveThursdays.push(new Date(today));
+        count++;
+      }
+    }
   }
-}
-selectedAccordion(val:string){
-  this.formGroup.patchValue({
-    selectedCategory:val
-  })
-}
-selectedLevelHeading(val:string){
-   this.formGroup.patchValue({
-    weeklyForm:({
-      sanskar:val
-    })
-   })
-}
-getFormattedDate(date:any) {
-  let year = new Date(date).getFullYear();
-  let month = new Date(date).getMonth()+1;
-  let selDate = new Date(date).getDate();
-  return selDate + '/' + month + '/' + year;
-}
-submitForm(form:any){
-  form.value.selectedDate = this.getFormattedDate(this.selectedDate);
-  form.value.vrutPrant = this.userData.prant;
-  form.value.vrutVibhag = this.userData.vibhag;
-  form.value.vrutJilla = this.userData.jilla;
-  form.value.vrutTaluka = this.userData.taluka;
-  form.value.vrutVasti = this.vrutVasti;
-  form.value.vrutShakha = this.vrutShakha;
-  if(this.selectedCategory=='W'){
-    form.value.monthlyForm = '';
-    if(form.value.weeklyForm.sanskar=='sanskar1'){
-      form.value.weeklyForm.sanskar2Group = '' ;
-   } else {
-    form.value.weeklyForm.sanskar1Group = '';
-   }
-  } else if(this.selectedCategory=='M'){
-    form.value.weeklyForm = '';
-    if(form.value.monthlyForm.sampark=='No'){
-      form.value.monthlyForm.swSankhya = '';
-      form.value.monthlyForm.visheshNondh = '' 
-   } 
-  } 
-  console.log(form.value);
-  if(this.formType =='Submit'){
-    this._apiService.postData('http://localhost:4000/shakhaVrut',form.value).subscribe((res)=>{
-      console.log(res)
-   })
-  }else{
-    this._apiService.udpateRecord(`http://localhost:4000/shakhaVrut/${this.formId}`,form.value).subscribe((res)=>{
-      console.log(res)
-   })
+  selectedAccordion(val: string) {
+    this.formGroup.patchValue({
+      selectedCategory: val,
+    });
   }
- 
-  this.formGroup.reset(); 
-}
+  selectedLevelHeading(val: string) {
+    this.formGroup.patchValue({
+      weeklyForm: {
+        sanskar: val,
+      },
+    });
+  }
+  getFormattedDate(date: any) {
+    let year = new Date(date).getFullYear();
+    let month = new Date(date).getMonth() + 1;
+    let selDate = new Date(date).getDate();
+    return selDate + '/' + month + '/' + year;
+  }
+  submitForm(form: any) {
+    form.value.selectedDate = this.getFormattedDate(this.selectedDate);
+    form.value.vrutPrant = this.userData.prant;
+    form.value.vrutVibhag = this.userData.vibhag;
+    form.value.vrutJilla = this.userData.jilla;
+    form.value.vrutTaluka = this.userData.taluka;
+    form.value.vrutVasti = this.vrutVasti;
+    form.value.vrutShakha = this.vrutShakha;
+    if (this.selectedCategory == 'W') {
+      form.value.monthlyForm = '';
+      if (form.value.weeklyForm.sanskar == 'sanskar1') {
+        form.value.weeklyForm.sanskar2Group = '';
+      } else {
+        form.value.weeklyForm.sanskar1Group = '';
+      }
+    } else if (this.selectedCategory == 'M') {
+      form.value.weeklyForm = '';
+      if (form.value.monthlyForm.sampark == 'No') {
+        form.value.monthlyForm.swSankhya = '';
+        form.value.monthlyForm.visheshNondh = '';
+      }
+    }
+    console.log(form.value);
+    if (this.formType == 'Submit') {
+      this._apiService
+        .postData('http://localhost:4000/shakhaVrut', form.value)
+        .subscribe((res) => {
+          console.log(res);
+        });
+    } else {
+      this._apiService
+        .udpateRecord(
+          `http://localhost:4000/shakhaVrut/${this.formId}`,
+          form.value
+        )
+        .subscribe((res) => {
+          console.log(res);
+        });
+    }
+
+    this.formGroup.reset();
+  }
 }
