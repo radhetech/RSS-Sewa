5aa3c9300c41d0ca15a73d5f9427696dff101c8e
Aryan Jangid <perryjangid@gmail.com>
2024-09-24 17:36:17 +0530

    Jilla Vrut Form Validation and Small UI Implementations

--- src/app/jilla-vrut/jilla-vrut.component.ts
+++ src/app/jilla-vrut/jilla-vrut.component.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-jilla-vrut',
   templateUrl: './jilla-vrut.component.html',
-  styleUrl: './jilla-vrut.component.scss'
+  styleUrl: './jilla-vrut.component.scss',
 })
 export class JillaVrutComponent {
-  aayamList:any = ['samajik','shiksha','swasthya','swavalamban'];
-  ayamChange(e:any){
+  aayamList: any = ['samajik', 'shiksha', 'swasthya', 'swavalamban'];
+  ayamChange(e: any) {
     // this.jillaForm.get('selAyam').value = e.value;
-    console.log(e)
+    console.log(e);
   }
-  jillaForm:FormGroup;
-  constructor(){
+  jillaForm: FormGroup;
+  isSubmit!: boolean;
+  constructor() {
     this.jillaForm = new FormGroup({
-      selAyam: new FormControl(''),
-      jBethak:new FormControl(''),
-      shakhaPravasNum: new FormControl(''),
-      sevaVastiPravasNum: new FormControl(''),
-      sevaKaryaPravasNum: new FormControl(''),
-      newSevaKaryaNum: new FormControl(''),
-      ayamSahNum:new FormControl('')
-    })
+      selAyam: new FormControl('', [Validators.required]),
+      jBethak: new FormControl('', [Validators.required]),
+      shakhaPravasNum: new FormControl('', [Validators.required]),
+      sevaVastiPravasNum: new FormControl('', [Validators.required]),
+      sevaKaryaPravasNum: new FormControl('', [Validators.required]),
+      newSevaKaryaNum: new FormControl('', [Validators.required]),
+      ayamSahNum: new FormControl('', [Validators.required]),
+    });
   }
-  submitJilla(form:FormGroup){
-    console.log(form)
+  submitJilla(form: FormGroup) {
+    this.isSubmit = true;
+    this.jillaForm.markAllAsTouched();
+    if (form.valid) {
+      console.log('Form Submit :',form);
+    } else {
+      console.error('Invalid Form :', form);
+    }
   }
 }
