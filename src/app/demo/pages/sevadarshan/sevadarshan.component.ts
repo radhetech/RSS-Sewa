bd3beac52d741e21acda9782f9908fae31afe2fa
Your Name <you@example.com>
2024-12-31 00:01:54 +0530

    seva darshan

---
+++ src/app/demo/pages/sevadarshan/sevadarshan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sevadarshan',
+  standalone: true,
+  imports: [],
+  templateUrl: './sevadarshan.component.html',
+  styleUrl: './sevadarshan.component.scss'
+})
+export class SevadarshanComponent {
+
+}
