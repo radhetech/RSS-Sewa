ff00faa502e6076440c597d718a4a5ecf5c984aa
Your Name <you@example.com>
2024-12-31 22:51:53 +0530

    latesh changes by vishal

--- src/app/demo/dashboard/dashboard.component.ts
+++ src/app/demo/dashboard/dashboard.component.ts
@@ -6,142 +6,150 @@ import { ApiService } from 'src/app/services/api.service';
 // project import
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SharedModule,RouterLink,RouterModule],
+  imports: [SharedModule, RouterLink, RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export default class DashboardComponent implements OnInit {
-  constructor(private apiService:ApiService){
-
-  }
+  constructor(private apiService: ApiService) {}
   ngOnInit() {
-
-      let temp = this.apiService.getUserData();
-      let userRole = temp.authorities[0];
-      if(userRole=='sdarshan'){
-        this.navList =  this.navList.filter((item:any)=>{
-           return item.id=='sevadarshan' || item.id=='pravas-list';
-        })
-      }else if(userRole=='taluka'){
-        this.navList =  this.navList.filter((item:any)=>{
-           return  item.id=='shakhavrut' || item.id=='upkram'|| item.id=='upkram'|| item.id=='sevakarya' || item.id=='vahivat' || item.id=='utsav';
-        })
-      } else if(userRole=='jilla'){
-   
-        this.navList =  this.navList.filter((item:any)=>{
-         return  item.id=='jilla' || item.id=='aheval' || item.id=='tempadmin'|| item.id=='pravas-list';
-       })
-      }
-     
-
+    let temp = this.apiService.getUserData();
+    let userRole = temp.authorities[0];
+    if (userRole == 'sdarshan') {
+      this.navList = this.navList.filter((item: any) => {
+        return item.id == 'sevadarshan' || item.id == 'pravas-list';
+      });
+    } else if (userRole == 'taluka') {
+      this.navList = this.navList.filter((item: any) => {
+        return (
+          item.id == 'shakhavrut' ||
+          item.id == 'upkram' ||
+          item.id == 'upkram' ||
+          item.id == 'sevakarya' ||
+          item.id == 'vahivat' ||
+          item.id == 'utsav'
+        );
+      });
+    } else if (userRole == 'jilla') {
+      this.navList = this.navList.filter((item: any) => {
+        return item.id == 'jilla' || item.id == 'aheval' || item.id == 'tempadmin' || item.id == 'pravas-list';
+      });
+    }
   }
 
   navList = [
     {
       id: 'shakhavrut',
       title: 'શાખા વૃત્ત',
-      description: ' (સેવા દિવસ ગુરૂવાર નું વૃત્ત માટે)',
+      description: ' (સેવા દિવસ ગુરૂવાર નું વૃત્ત)',
       design: 'col-md-3',
-      link:'/home/shakhavrut',
-      img:'../../../assets/images/shakha-vrut-circle.png',
-      cardClass:'dashboard-card',
-      headerClass:'dashboard-card-header',
-      blockClass:'dashboard-card-block',
+      link: '/home/shakhavrut',
+      img: '../../../assets/images/shakha-vrut-circle.png',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
     {
       id: 'upkram',
       title: 'સેવા ઉપક્રમ',
-      description: '(સેવા ઉપક્રમની વિગતો ભરવા માટે)',
+      description: '(સેવા ઉપક્રમની વિગતો ભરવા)',
       design: 'col-md-3',
-      link:'/home/sevaupkram',
-       img:'../../../assets/images/Shikshan1.webp',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/sevaupkram',
+      img: '../../../assets/images/seva-upkram.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'sevakarya',
+    {
+      id: 'sevakarya',
       title: 'સેવા કાર્ય',
-      description: '(સેવા કાર્યની વિગતો ભરવા માટે)',
+      description: '(સેવા કાર્યની વિગતો ભરવા)',
       design: 'col-md-3',
-      link:'/home/sevakary',
-       img:'../../../assets/images/sevabharti-circle.png',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/sevakary',
+      img: '../../../assets/images/seva-karya-1.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'utsav',
+    {
+      id: 'utsav',
       title: 'ઉત્સવ વૃત',
-      description: '(ઉત્સવનું વૃત્ત આપવા માટે)',
+      description: '(ઉત્સવનું વૃત્ત આપવા)',
       design: 'col-md-3',
-      link:'/home/utsavvrut',
-       img:'../../../assets/images/utsav.jpeg',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/utsavvrut',
+      img: '../../../assets/images/utsav.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'sevadarshan',
+    {
+      id: 'sevadarshan',
       title: 'સેવા દર્શન',
-      description: '(સેવા દર્શનની વિગતો ભરવા માટે)',
+      description: '(સેવા દર્શનની વિગતો ભરવા)',
       design: 'col-md-3',
-      link:'/home/sevadarshan',
-       img:'../../../assets/images/sevadarshan.jpeg',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/sevadarshan',
+      img: '../../../assets/images/sevadarshan.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'pravas-list',
+    {
+      id: 'pravas-list',
       title: 'સેવા દર્શન પ્રવાસ યાદી ',
-      description: '',
+      description: '(સેવા દર્શન માટે જનારની યાદી)',
       design: 'col-md-3',
-      link:'/home/pravaslist',
-       img:'../../../assets/images/sevadarshan.jpeg',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/pravaslist',
+      img: '../../../assets/images/sevabharti-circle.png',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'jilla',
+    {
+      id: 'jilla',
       title: 'જિલ્લા વૃત',
       description: '(જિલ્લા વૃત માટે)',
       design: 'col-md-3',
-      link:'/home/jillavrut',
-       img:'../../../assets/images/shakha-vrut-circle.png',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/jillavrut',
+      img: '../../../assets/images/shakha-vrut-circle.png',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'vahivat',
+    {
+      id: 'vahivat',
       title: 'વહીવટી',
       description: '(વહીવટી કામ માટે)',
       design: 'col-md-3',
-      link:'/home/vahivat',
-       img:'../../../assets/images/profile.avif',
-       cardClass:'dashboard-card',
-       headerClass:'panel-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/vahivat',
+      img: '../../../assets/images/profile.avif',
+      cardClass: 'dashboard-card',
+      headerClass: 'panel-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'aheval',
-      title: 'અહેવાલ',
-      description: '(માસિક અને વાર્ષિક અહેવાલ)',
+    {
+      id: 'aheval',
+      title: 'વૃત્ત',
+      description: '(માસિક અને વાર્ષિક વૃત્ત)',
       design: 'col-md-3',
-      link:'/home/report',
-       img:'../../../assets/images/shakha-vrut-circle.png',
-       cardClass:'dashboard-card',
-       headerClass:'panel-header',
-       blockClass:'dashboard-card-block',
+      link: '/home/report',
+      img: '../../../assets/images/vrut.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'panel-header',
+      blockClass: 'dashboard-card-block'
     },
-    { id: 'tempadmin',
+    {
+      id: 'tempadmin',
       title: 'સેવા દર્શન યુઝર્સ માટે',
-      description: 'for seva darshan',
+      description: ' (ફક્ત સેવા દર્શન કરવા જનાર માટે)',
       design: 'col-md-3',
-      link:'/home/tempadmin',
-       img:'../../../assets/images/Shikshan1.webp',
-       cardClass:'dashboard-card',
-       headerClass:'dashboard-card-header',
-       blockClass:'dashboard-card-block',
-    },
+      link: '/home/tempadmin',
+      img: '../../../assets/images/seva-darshan.jpeg',
+      cardClass: 'dashboard-card',
+      headerClass: 'dashboard-card-header',
+      blockClass: 'dashboard-card-block'
+    }
   ];
 }
